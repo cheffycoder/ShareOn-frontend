00614f3399d4c59d10c99a5a652655fdc197fc84
Shivam Bandral <shivambandral@gmail.com>
2022-07-26 23:57:56 +0530

    File Upload functionality Done

--- index.js
+++ index.js
@@ -1,112 +1,117 @@
-const dropZone = document.querySelector('.drop-zone');
-const fileInput = document.querySelector('#file-input');
-const browseBtn = document.querySelector('.browse-button')
+const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#file-input");
+const browseBtn = document.querySelector(".browse-button");
 
-const host = "https://innshare.herokuapp.com/"
+const host = "https://innshare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
 // const uploadURL = `${host}api/files`; //--> will use this later for uploading using email.
 
-
-// MDN JS give us this Web API called DragEvent which senses if somthing is dragged over the droptarget
-dropZone.addEventListener("dragover", (e)=>{
-    // This is done because the default behaviour on this action is to download and we are preventing it.
-    e.preventDefault();
-    /* 
-        dropZone.classList.add("dragged");
-
-        Adding the dragged class in the dropZone to make sure the animation run, 
-        but this isn't efficient because the animation doesn't goes off after we come out of dropZone.
-    */
-
-    if(!dropZone.classList.contains("dragged")){
-        dropZone.classList.add("dragged");
-    }
+// Adding the button to get the click for fileInput which is displayed none, and called click on hidden input field which will open file menu.
+browseBtn.addEventListener("click", () => {
+  // This click will trigger the fileMenu opener and as soon as we select a file, change event listener on fileInput is triggered to upload the selected file.
+  fileInput.click();
 });
 
+//--------------- Animation Setup ----------->
 
-dropZone.addEventListener("dragleave", (e)=>{
-    e.preventDefault();
+// Adding the animation to the dropzone by adding an extra class called draggable to the dropzone
+dropZone.addEventListener("dragover", (e) => {
+  // This is done because the default behaviour on this action is to download and we are preventing it.
+  e.preventDefault();
+  /**
+   * dropZone.classList.add("dragged");
+   *
+   * Adding the dragged class in the dropZone to make sure the animation run,
+   * but this isn't efficient because the animation doesn't goes off after we come out of dropZone.
+   *
+   * Thus, adding dragged class only when there is no dragged class already existing.
+   */
+  if (!dropZone.classList.contains("dragged")) {
+    dropZone.classList.add("dragged");
+  }
+});
 
-    dropZone.classList.remove("dragged");
-})
+// Stopping the animation by removing the class as soon as drag element is left
+dropZone.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  dropZone.classList.remove("dragged");
+});
 
+/** As soon as the file is dropped then remove the animation class,
+ * then get the files from dropEvent using e.dataTransfer.files
+ * and save them to the inputHTMLElement files object.
+ * */
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  dropZone.classList.remove("dragged");
 
-dropZone.addEventListener("drop", (e)=>{
-    e.preventDefault();
-    dropZone.classList.remove("dragged");
+  const fileReceived = e.dataTransfer.files;
 
-    // console.log(e.dataTransfer.files.length);
-    const fileReceived = e.dataTransfer.files;
+  // This check is there to check if files are really being dropped or not.
+  if (fileReceived.length) {
+    fileInput.files = fileReceived;
+    // Get the first file out of all the dropped files and upload only that one.
+    const firstFile = fileInput.files[0];
+    uploadFile(firstFile);
+  }
+});
 
-    console.log(fileReceived);
+// Also send the upload request other than drop when someone selects a file from browse.
+fileInput.addEventListener("change", () => {
+  const receivedFile = fileInput.files[0];
+  uploadFile(receivedFile);
+});
 
-    // This check is there to check if files are really being dropped or not.
-    if(fileReceived.length){
-        fileInput.files = fileReceived;
-        // If yes then upload them to the server and send a post request
-        uploadFile();
-    }   
-})
+/**
+ * Upload the file by making an XHR(XML HTTP Request) post method call using the formObject.
+ * For that we have to give the file as a formData where the key for the fileUploaded is "myfile",
+ * this key is agreed upon by both backend and frontend.
+ *
+ * Thus, make a new formDataInstance and then call the append method on it to append your file to that formData.
+ */
 
+/**
+ * This will be triggered once the call will go and will take care of every step of the API call.
+ * xhr.readyState is the value that will take care of the State
+ * XMLHttpRequest.DONE is the final stage telling us that request is success and response is there.
+ * We can access the response using xhr.response
+ */
+const uploadFile = (file) => {
+  const formData = new FormData();
+  formData.append("myfile", file);
 
+  const xhr = new XMLHttpRequest();
 
-browseBtn.addEventListener("click", (e)=>{
-    fileInput.click();
-})
-
-
-// Uploading a file using xhr
-const uploadFile = () => {
-
-    const file = fileInput.files[0];
-
-    // This will create a new form object which we will upload using post method. We have to enter our file in the form data to post.
-    const formData = new FormData();
-    formData.append("myfile",file); // This check was there when we made the post request in backend. Thus, have to pass the first argument as myfile
-
-    // Uploading using xhr request, Adding event listeners to it.
-    const xhr = new XMLHttpRequest();
-
-    // This will be triggered once the file will be uploaded
-    xhr.onreadystatechange= ()=>{
-        console.log(xhr.readyState);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.response);
     }
+  };
 
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
 
-    xhr.open("POST", uploadURL);
-    xhr.send(formData);
-}
+// fileInput.addEventListener("change", () => {
+//     console.log("shivam")
+//   uploadFile();
+// });
 
+// /*
+//     Creating Progress Bar
+// */
 
-fileInput.addEventListener("change", ()=>{
-    uploadFile();
-})
+// const bgProgress = document.querySelector(".bg-progress");
+// const percentDiv = document.querySelector("#percent");
+// const progressBar = document.querySelector(".progress-bar");
+// const progressContainer = document.querySelector("");
 
+// const updateProgress = (e)=>{
+//     const percentage = Math.round((e.loaded/e.total) * 100);
+//     bgProgress.style.width = `${percent}%`;
+//     percentDiv.innerText= percent;
 
+//     progressBar.style.transform = `scaleX(${percent/100})`
+// }
 
-
-/* 
-    Creating Progress Bar
-*/
-
-const bgProgress = document.querySelector(".bg-progress");
-const percentDiv = document.querySelector("#percent");
-const progressBar = document.querySelector(".progress-bar");
-const progressContainer = document.querySelector("");
-
-
-const updateProgress = (e)=>{
-    const percentage = Math.round((e.loaded/e.total) * 100);
-    bgProgress.style.width = `${percent}%`;
-    percentDiv.innerText= percent;
-
-    progressBar.style.transform = `scaleX(${percent/100})`
-}
-
-
-
-// Done till Adding drag and drop
-
-
-
-
+// // Done till Adding drag and drop
