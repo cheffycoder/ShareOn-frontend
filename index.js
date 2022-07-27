a0c2ed88649638d54ec1797e372fe8652bfba66a
Shivam Bandral <shivambandral@gmail.com>
2022-07-27 19:51:49 +0530

    Progess Container Completed

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
-const dropZone = document.querySelector(".drop-zone");
-const fileInput = document.querySelector("#file-input");
-const browseBtn = document.querySelector(".browse-button");
+const dropZoneElem = document.querySelector(".drop-zone");
+const fileInputElem = document.querySelector("#file-input");
+const browseBtnElem = document.querySelector(".browse-button");
+
+const progressContainerElem = document.querySelector(".progress-container");
+const uploadProgressElem = document.querySelector(".upload-progress");
+const percentageValueElem = document.getElementById("percent-value");
 
 const host = "https://innshare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
 // const uploadURL = `${host}api/files`; //--> will use this later for uploading using email.
 
-// Adding the button to get the click for fileInput which is displayed none, and called click on hidden input field which will open file menu.
-browseBtn.addEventListener("click", () => {
-  // This click will trigger the fileMenu opener and as soon as we select a file, change event listener on fileInput is triggered to upload the selected file.
-  fileInput.click();
+// Adding the button to get the click for fileInputElem which is displayed none, and called click on hidden input field which will open file menu.
+browseBtnElem.addEventListener("click", () => {
+  // This click will trigger the fileMenu opener and as soon as we select a file, change event listener on fileInputElem is triggered to upload the selected file.
+  fileInputElem.click();
 });
 
 //--------------- Animation Setup ----------->
 
-// Adding the animation to the dropzone by adding an extra class called draggable to the dropzone
-dropZone.addEventListener("dragover", (e) => {
+// Adding the animation to the dropzoneElem by adding an extra class called draggable to the dropzoneElem
+dropZoneElem.addEventListener("dragover", (e) => {
   // This is done because the default behaviour on this action is to download and we are preventing it.
   e.preventDefault();
   /**
-   * dropZone.classList.add("dragged");
+   * dropZoneElem.classList.add("dragged");
    *
-   * Adding the dragged class in the dropZone to make sure the animation run,
-   * but this isn't efficient because the animation doesn't goes off after we come out of dropZone.
+   * Adding the dragged class in the dropZoneElem to make sure the animation run,
+   * but this isn't efficient because the animation doesn't goes off after we come out of dropZoneElem.
    *
    * Thus, adding dragged class only when there is no dragged class already existing.
    */
-  if (!dropZone.classList.contains("dragged")) {
-    dropZone.classList.add("dragged");
+  if (!dropZoneElem.classList.contains("dragged")) {
+    dropZoneElem.classList.add("dragged");
   }
 });
 
 // Stopping the animation by removing the class as soon as drag element is left
-dropZone.addEventListener("dragleave", (e) => {
+dropZoneElem.addEventListener("dragleave", (e) => {
   e.preventDefault();
-  dropZone.classList.remove("dragged");
+  dropZoneElem.classList.remove("dragged");
 });
 
 /** As soon as the file is dropped then remove the animation class,
  * then get the files from dropEvent using e.dataTransfer.files
  * and save them to the inputHTMLElement files object.
  * */
-dropZone.addEventListener("drop", (e) => {
+dropZoneElem.addEventListener("drop", (e) => {
   e.preventDefault();
-  dropZone.classList.remove("dragged");
+  dropZoneElem.classList.remove("dragged");
 
   const fileReceived = e.dataTransfer.files;
 
   // This check is there to check if files are really being dropped or not.
   if (fileReceived.length) {
-    fileInput.files = fileReceived;
+    fileInputElem.files = fileReceived;
     // Get the first file out of all the dropped files and upload only that one.
-    const firstFile = fileInput.files[0];
+    const firstFile = fileInputElem.files[0];
     uploadFile(firstFile);
   }
 });
 
 // Also send the upload request other than drop when someone selects a file from browse.
-fileInput.addEventListener("change", () => {
-  const receivedFile = fileInput.files[0];
+fileInputElem.addEventListener("change", () => {
+  const receivedFile = fileInputElem.files[0];
   uploadFile(receivedFile);
 });
 
@@ -77,6 +81,9 @@ fileInput.addEventListener("change", () => {
  * We can access the response using xhr.response
  */
 const uploadFile = (file) => {
+  // Showing the progress element as soon as someone uploads a file.
+  updateContainerElem(progressContainerElem, "show");
+
   const formData = new FormData();
   formData.append("myfile", file);
 
@@ -84,34 +91,35 @@ const uploadFile = (file) => {
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      console.log(xhr.response);
+      // Showing the download link to the user, response returns an object with file property containing the link.
+      showDownloadLink(JSON.parse(xhr.response));
     }
   };
+
+  // ---------------- Monitoring uplaod progress  ------------>
+
+  xhr.upload.addEventListener("progress", updateProgress);
 
   xhr.open("POST", uploadURL);
   xhr.send(formData);
 };
 
-// fileInput.addEventListener("change", () => {
-//     console.log("shivam")
-//   uploadFile();
-// });
+const updateContainerElem = (element, action) => {
+  element.style.display = action === "show" ? "flex" : "none";
+};
 
-// /*
-//     Creating Progress Bar
-// */
+const updateProgress = (event) => {
+  const uploadPercentage = Math.round((event.loaded / event.total) * 100);
 
-// const bgProgress = document.querySelector(".bg-progress");
-// const percentDiv = document.querySelector("#percent");
-// const progressBar = document.querySelector(".progress-bar");
-// const progressContainer = document.querySelector("");
+  percentageValueElem.textContent = uploadPercentage;
+  uploadProgressElem.style.width = `${uploadPercentage}%`;
+};
 
-// const updateProgress = (e)=>{
-//     const percentage = Math.round((e.loaded/e.total) * 100);
-//     bgProgress.style.width = `${percent}%`;
-//     percentDiv.innerText= percent;
+const showDownloadLink = ({ file }) => {
+  console.log(file);
+  // Closing the progressContainer as soon as the file is fully uploaded
+  updateContainerElem(progressContainerElem, "hide");
 
-//     progressBar.style.transform = `scaleX(${percent/100})`
-// }
-
-// // Done till Adding drag and drop
+  // Also show the link Expiry text with the download link
+  // Also show the send link through email element
+};
